8e442aa30908ab02ab912799675947829f8b8065
Mike Nightingale <michael.nightingale@candide.eu>
2021-03-09 09:47:51 +0000

    lhrc server

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,7 +1,9 @@
 module.exports = {
   ci: {
     upload: {
-      target: 'temporary-public-storage',
+      target: 'lhci',
+      serverBaseUrl: "http://localhost:9001/",
+      token: "8c2186a2-cb03-4b67-8b89-0667176e4c81",
     },
   },
 };
